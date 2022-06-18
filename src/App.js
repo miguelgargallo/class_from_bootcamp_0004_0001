d8b557d5e5669e06ab44b0683ecee2f79d7e7a19
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-18 08:11:21 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    Date: '2022-01-01T01:10:01.001Z',
+    important: true,
+  },
+    id: 2,
+    content: 'HTML not easy',
+    Date: '2022-02-01T01:10:01.001Z',
+    important: true,
+  },
+    id: 3,
+    content: 'HTML equals easy',
+    Date: '2022-03-01T01:10:01.001Z',
+    important: true,
+  }
+]
+
+export default function App() {
+  return <h1>Clase 4 del Bootcamp</h1>;
+}
