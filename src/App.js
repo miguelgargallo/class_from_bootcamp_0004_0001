7efa2ffa24621e0c9911d9c0fa2ff815ada565a9
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-18 08:30:11 +0200

    class_from_bootcamp_0004_0006 class_from_bootcamp_0004_0006

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ const notes = [
   {
     id: 1,
     content: "HTML is easy",
-    Date: "2022-01-01T01:10:01.001Z",
+    date: "2022-01-01T01:10:01.001Z",
     important: true
   },
   {
     id: 2,
     content: "HTML not easy",
-    Date: "2022-02-01T01:10:01.001Z",
+    date: "2022-02-01T01:10:01.001Z",
     important: true
   },
   {
